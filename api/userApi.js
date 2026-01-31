e707f5da7913f721844e2c20e8ed126c6e336444
Harini-456 <harinisarav456@gmail.com>
2026-01-31 11:10:59 +0530

    User registration and login feature v0.1

---
+++ api/userApi.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const express = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+const router  = express.Router()
+
+router.post('/signup', async(req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const role = req.body.role
+    const password = req.body.password
+
+  if (!email || !password) {
+        return res.json({"message":"invalid request fields"})
+    }
+  else if (role!= "MANAGER" && role!= "EMPLOYEE"){
+     return res.json({"message":"invalid request role"})
+  }
+  else if(password.length <= 5){
+     return res.json({"message":"invalid request password"})
+  }
+
+  
+    const usercheck = await User.findOne({email:email})
+    console.log("userCheck: ",usercheck)
+        if(usercheck){
+            return res.json({"mesage":"email already exists"})
+        }
+
+  const hashPassword = await bcrypt.hash(password, 10)
+    const user = new User({
+        name: name,
+        email: email,
+        password: hashPassword,
+        role: role,
+    })
+    await user.save()
+    return res.json({"message":"success"})
+}) 
+
+router.post("/login", async(req,res) => {
+    console.log(req.body)
+    const user = await User.findOne({email: req.body.email})
+    if(!user){
+        return res.json({message: "Email is invalid"})
+    }
+    const isPasswordMatching = await bcrypt.compare(req.body.password , user.password)
+    if(!isPasswordMatching){
+        return res.json({"message":"password invalid"})
+    }
+    try{
+    const token = jwt.sign(
+        {user: user._id},
+        process.env.SECRET_CODE,
+        {expiresIn:"1h"}
+    )
+    return res.json({message:"login sucessfull",token: token})
+}
+catch(err){
+    console.log(err)
+    return res.json({"message":"Server error"})
+}
+})
+module.exports = router
---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const userSchema =mongoose.Schema({
+    name: String,
+    email: String,
+    role: String,
+    password: String
+})
+
+const User =  mongoose.model("user",userSchema)
+
+module.exports = User
